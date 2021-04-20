3cfd7274d31187ca90fb297e04c5dabe560142bf
ball <nattawatx86@gmail.com>
2021-04-20 13:48:41 +0700

    sidebar

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+function openSideBar() {
+  document.getElementsByClassName("menu")[0].style.width = "100%"
+}
+
+function closeSideBar() {
+  document.getElementsByClassName("menu")[0].style.width = "0"
+}
